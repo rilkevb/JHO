08093c3ad9fe7e14502b586a03984b8456175860
Matthew Villwock & rilkevb <pair+mtvillwock.rilkevb@devbootcamp.com>
2015-04-21 15:34:13 -0700

    generate users and boards controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
+  get 'user/destroy'
+
+  get 'board/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
