6dff8ddd7d43d755bd9d8934464fbba5475c75c6
Matthew Villwock & rilkevb <pair+mtvillwock.rilkevb@devbootcamp.com>
2015-04-21 15:58:33 -0700

    made user route plural (users)

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  get 'users/new'
+  post 'users/'
+  get 'users/destroy'
 
-  get 'user/destroy'
-
-  get 'board/show'
+  get 'boards/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
