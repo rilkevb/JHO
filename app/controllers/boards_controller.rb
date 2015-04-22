3ca7dc9035a7af2b0e00b55bd94fc39fa0b0c7b3
Matthew Villwock <mtvillwock@gmail.com>
2015-04-21 17:34:58 -0700

    create index and show templates for users and boards, write controller logic to query database for necessary information to render to view

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,12 @@
 class BoardsController < ApplicationController
+  def index
+    @user = User.find_by(id: params[:user_id])
+    @boards = @user.boards
+  end
+
   def show
-    @user = User.find_by(id: params[:id])
-    @board = Board.find_by(id: params[:board_id])
+    @user = User.find_by(id: params[:user_id])
+    @board = Board.find_by(id: params[:id])
+    @lists = @board.lists
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
   def new
   end
 
