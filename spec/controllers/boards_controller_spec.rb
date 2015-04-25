3a32d4d0071c7043d436feaa7ea21930d8986be8
Matthew Villwock <mtvillwock@gmail.com>
2015-04-25 00:15:51 -0700

    5 tests passing for boards_controller_spec, but for some reason they do not pass when the 'p' statements are removed. Seems to be having trouble with @lists[0].cards call

--- spec/controllers/boards_controller_spec.rb
+++ spec/controllers/boards_controller_spec.rb
@@ -4,12 +4,14 @@ RSpec.describe BoardsController, :type => :controller do
 
   let(:user) { User.create(name: "Matt") }
   let(:board_1) { user.boards.create(name: "Software Developer Job Hunt", user_id: user.id) }
-  let(:list_1) { List.create(board_id: board_1.id, name: "Organizations of Interest", position_id: 1) }
-  let(:list_2) { List.create(board_id: board_1.id, name: "Finding Advocate", position_id: 2) }
-  let(:list_3) { List.create(board_id: board_1.id, name: "Initial Application", position_id: 3) }
-  let(:list_4) { List.create(board_id: board_1.id, name: "Cultural Interview", position_id: 4) }
-  let(:list_5) { List.create(board_id: board_1.id, name: "Tech Screen/Code Challenge", position_id: 5) }
-  let(:list_6) { List.create(board_id: board_1.id, name: "Onsite", position_id: 6) }
+  let(:list_1) { board_1.lists.create(board_id: board_1.id, name: "Organizations of Interest", position_id: 1) }
+  let(:list_2) { board_1.lists.create(board_id: board_1.id, name: "Finding Advocate", position_id: 2) }
+  let(:list_3) { board_1.lists.create(board_id: board_1.id, name: "Initial Application", position_id: 3) }
+  let(:list_4) { board_1.lists.create(board_id: board_1.id, name: "Cultural Interview", position_id: 4) }
+  let(:list_5) { board_1.lists.create(board_id: board_1.id, name: "Tech Screen/Code Challenge", position_id: 5) }
+  let(:list_6) { board_1.lists.create(board_id: board_1.id, name: "Onsite", position_id: 6) }
+
+  let(:lists) { board_1.lists }
 
   let(:card_1) { Card.create(list_id: 1, organization_name: "Google") }
   let(:card_2) { Card.create(list_id: 1, organization_name: "ZenPayroll") }
@@ -19,7 +21,7 @@ RSpec.describe BoardsController, :type => :controller do
   let(:card_6) { Card.create(list_id: 2, organization_name: "Make") }
   let(:card_7) { Card.create(list_id: 3, organization_name: "Amazon") }
 
-  let(:card_8) { list1.cards.new organization_name: "Dev Bootcamp"}
+  let(:card_8) { user.boards.first.lists.first.cards.new organization_name: "Dev Bootcamp"}
 
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
@@ -46,27 +48,54 @@ RSpec.describe BoardsController, :type => :controller do
 
   describe "GET #show" do
     it "responds successfully with an HTTP 200 status code" do
+      p user
+      p board_1
+      p list_1
+      p card_1
+      p card_8
       get :show, { :id => board_1.id, :user_id => user.id  }
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "renders the show template" do
+      p user
+      p board_1
+      p list_1
+      p card_1
+      p card_8
       get :show, { :id => board_1.id, :user_id => user.id  }
       expect(response).to render_template("show")
     end
 
     it "finds and loads the current user" do
+      p user
+      p board_1
+      p list_1
+      p card_1
+      p card_8
+      get :show, { :id => board_1.id, :user_id => user.id  }
       expect(assigns(:user)).to match(user)
     end
 
-    it "finds loads the current board" do
+    it "finds and loads the current board" do
+      p user
+      p board_1
+      p list_1
+      p card_1
+      p card_8
+      get :show, { :id => board_1.id, :user_id => user.id  }
       expect(assigns(:board)).to match(board_1)
     end
 
-    it "loads all of the lists into @lists" do
-      get :show
-      expect(assigns(:lists)).to match_array([list1, list2, list3, list4, list5, list6])
+    it "loads all of the current board's lists" do
+      p user
+      p board_1
+      p list_1
+      p card_1
+      p card_8
+      get :show, { :id => board_1.id, :user_id => user.id  }
+      expect(assigns(:lists)).to match_array(lists)
     end
   end
 end
