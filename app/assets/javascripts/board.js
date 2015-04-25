7010ff9fba61ca6695e308e89ec17ac9e6ead8e5
Matthew Villwock & rilkevb <pair+mtvillwock.rilkevb@devbootcamp.com>
2015-04-24 17:36:05 -0700

    Drag and Drop listeners
    Add listener and callback for mouseup event to grab card id when card is dragged to new list.
    
    Add listener and callback for drop event to get list id, board id, and user id in order to pass parameters to server for update card route.
    
    Edit board#show ERB file to have appropriate classes for jQuery selections.

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
 
 function bindEvents() {
   $(".add-card").on("submit", "form", addNewCard);
+  // mouse down listener
+  $('.list').mouseup(".card", findCardId);
+  $('.list').droppable( {drop: findListId} )
 }
 
 function addNewCard(event) {
@@ -14,6 +17,7 @@ function addNewCard(event) {
     url: that.action,
     type: "POST",
     data: {organization_name: $(that).children()[0].value}
+    // Will need to edit this is we add more fields to this form
   }).done( function(response) {
     console.log("done :", response)
     var newCard = "<li class='ui-state-default' id='card" + response.id + "'>" +
@@ -23,4 +27,26 @@ function addNewCard(event) {
   }).fail( function(response) {
     console.log("failed :", response)
   });
+}
+
+function findCardId(event) {
+  cardId = $(event.target).attr('id').slice(4);  //BE AWARE OF GLOBAL VARIABLE
+}
+
+function findListId(event) {
+  event.preventDefault();
+  var that = this;
+  var listId = that.id;
+  var boardId =  $('.board').attr('id');
+
+  debugger;
+  $.ajax( {
+    url: "users/1/boards/" + boardId + "/lists/" + listId + "cards/" + cardId,
+    type: "PUT",
+    data: {list_id: listId, card_id: cardId, board_id: boardId}
+  }).done(function(response) {
+    console.log(response, "success")
+  }).fail(function(response) {
+    console.log(response, "fail")
+  })
 }
