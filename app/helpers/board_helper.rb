122b8118c72aa64ed25937c922c31436e0a8993e
Matthew Villwock & rilkevb <pair+mtvillwock.rilkevb@devbootcamp.com>
2015-04-30 13:29:00 -0700

    modified lists in generate_board_lists helper.  modified seed file to use helper.   works correctly

--- app/helpers/board_helper.rb
+++ app/helpers/board_helper.rb
@@ -1,2 +1,12 @@
 module BoardHelper
+  def generate_board_lists(board)
+    list_names = ["Organizations of Interest", "Finding Advocate", "Found Advocate",
+    "Preparing Application", "Application Submitted", "Cultural Interview Prep",
+    "Cultural Interview", "Code Challenge", "Tech Screen Prep", "Tech Screen",
+    "Onsite Interview Prep", "Onsite Interview", "Negotiation", "Outcome"]
+
+    list_names.each_with_index do |name,index|
+      board.lists.create(name: name, position_id: (index+1) )
+    end
+  end
 end
