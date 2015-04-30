50cfb0d48c58c8a253403481d96289acc49c9b64
Matthew Villwock & rilkevb <pair+mtvillwock.rilkevb@devbootcamp.com>
2015-04-30 13:30:08 -0700

    modified lists in generate_board_lists helper.  modified seed file to use helper.   works correctly

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,4 +10,14 @@ class BoardsController < ApplicationController
     @lists = @board.lists
     @card = @lists[0].cards.new(organization_name: params[:organization_name])
   end
+
+  def create
+    user = User.find_by(id: params[:id])
+    board = user.boards.create(name: params[:name])
+    if board.save
+      generate_board_lists(board)
+    else
+      render json: {error: "board failed to save"}
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+include BoardHelper
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -9,17 +10,20 @@
 matt = User.create(name: "Matt")
 
 board_1 = Board.create(name: "Software Developer Job Hunt", user_id: User.first.id)
-list_1 = List.create(board_id: Board.first.id, name: "Organizations of Interest", position_id: 1)
-list_2 = List.create(board_id: Board.first.id, name: "Finding Advocate", position_id: 2)
-list_3 = List.create(board_id: Board.first.id, name: "Initial Application", position_id: 3)
-list_4 = List.create(board_id: Board.first.id, name: "Cultural Interview", position_id: 4)
-list_5 = List.create(board_id: Board.first.id, name: "Tech Screen/Code Challenge", position_id: 5)
-list_6 = List.create(board_id: Board.first.id, name: "Onsite", position_id: 6)
 
-card_1 = Card.create(list_id: 1, organization_name: "Google")
-card_2 = Card.create(list_id: 1, organization_name: "ZenPayroll")
-card_3 = Card.create(list_id: 2, organization_name: "Adobe")
-card_4 = Card.create(list_id: 2, organization_name: "Facebook")
-card_5 = Card.create(list_id: 2, organization_name: "Wired")
-card_6 = Card.create(list_id: 2, organization_name: "Make")
-card_7 = Card.create(list_id: 3, organization_name: "Amazon")
+generate_board_lists(board_1)
+
+# list_1 = List.create(board_id: Board.first.id, name: "Organizations of Interest", position_id: 1)
+# list_2 = List.create(board_id: Board.first.id, name: "Finding Advocate", position_id: 2)
+# list_3 = List.create(board_id: Board.first.id, name: "Initial Application", position_id: 3)
+# list_4 = List.create(board_id: Board.first.id, name: "Cultural Interview", position_id: 4)
+# list_5 = List.create(board_id: Board.first.id, name: "Tech Screen/Code Challenge", position_id: 5)
+# list_6 = List.create(board_id: Board.first.id, name: "Onsite", position_id: 6)
+
+# card_1 = Card.create(list_id: 1, organization_name: "Google")
+# card_2 = Card.create(list_id: 1, organization_name: "ZenPayroll")
+# card_3 = Card.create(list_id: 2, organization_name: "Adobe")
+# card_4 = Card.create(list_id: 2, organization_name: "Facebook")
+# card_5 = Card.create(list_id: 2, organization_name: "Wired")
+# card_6 = Card.create(list_id: 2, organization_name: "Make")
+# card_7 = Card.create(list_id: 3, organization_name: "Amazon")
