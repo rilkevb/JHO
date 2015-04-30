3960be98428197096fc9d25e9353b0a95067fe5a
Matthew Villwock <mtvillwock@gmail.com>
2015-04-29 22:51:41 -0700

    edit update rate to use either or for conditional logic to create a movement or update card fields

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,34 +7,35 @@ class CardsController < ApplicationController
   end
 
   def create
-    # p "*="*50
-    # p "PARAMS ARE: "
-    # p params
-    # p "params[:organization_name]: "
-    # p params[:organization_name]
-    # @card = Card.new(card_params)
-    @card = Card.new(list_id: 1, organization_name: params[:organization_name])
-    if @card.save
-      render json: @card
+    card = Card.new(list_id: 1, organization_name: params[:organization_name])
+    if card.save
+      render json: card
     else
-      #Need to add validation to prevent nill card being created...
+      #Need to add validation to prevent nil card being created...
       render json: { error: "card failed to create"}
     end
   end
 
   def update
-    @card = Card.find_by(id: params[:card_id])
-    @card.list_id = params[:list_id]
-    if @card.save
-      @movement = @card.movements.new(current_list: params[:list_id], card_id: @card.id)
-      if @movement.save
-        render nothing: true
+    card = Card.find_by(id: params[:card_id])
+    if params[:list_id]
+      card.list_id = params[:list_id]
+      if card.save
+        movement = card.movements.new(current_list: params[:list_id], card_id: card.id)
+        if movement.save
+          render nothing: true
+        else
+          # movement fail
+        end
       else
-        # movement fail
+        # card save fail
       end
     else
-      # card save fail
+      card.update_attributes(organization_name: params[:organization_name]
+                              organization_summary: params[:organization_summary])
+      render json: card
     end
+    # need to separate this into two separate custom routes
   end
 
   def destroy
