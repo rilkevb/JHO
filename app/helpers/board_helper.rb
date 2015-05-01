5a9d74ef39f0e96ddc251b7c7d8e3878f484e619
Matthew Villwock & rilkevb <pair+mtvillwock.rilkevb@devbootcamp.com>
2015-05-01 11:31:53 -0700

    updated board helper for new simplified collection of lists.  changed corresponding board css

--- app/helpers/board_helper.rb
+++ app/helpers/board_helper.rb
@@ -1,9 +1,8 @@
 module BoardHelper
   def generate_board_lists(board)
-    list_names = ["Organizations of Interest", "Finding Advocate", "Found Advocate",
-    "Preparing Application", "Application Submitted", "Cultural Interview Prep",
-    "Cultural Interview", "Code Challenge", "Tech Screen Prep", "Tech Screen",
-    "Onsite Interview Prep", "Onsite Interview", "Negotiation", "Outcome"]
+    list_names = ["Organizations of Interest", "Find Advocate",
+    "Application", "Cultural", "Code Challenge", "Tech Screen",
+    "Onsite", "Negotiation", "Outcome"]
 
     list_names.each_with_index do |name,index|
       board.lists.create(name: name, position_id: (index+1) )
