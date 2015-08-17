d72998f31c2aa22052193341acab49a913916924
Matthew Villwock <mtvillwock@gmail.com>
2015-08-16 20:24:43 -0700

    only create, update, destroy actions, add user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 class UsersController < ApplicationController
-  def index
 
+  def create
+    user = User.new(user_params)
+    if user.save!
+      render json: user
+    else
+      render json: { error: "user creation failed"}
+    end
   end
 
-  def show
-    @user = User.find_by(id: params[:id])
-  end
-
-  def new
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: { error: "user update failed"}
+    end
   end
 
   def destroy
+    user = User.find(params[:id])
+    if user.destroy!
+      render json: { success: "user destroyed"}
+    else
+      render json: { error: "user not destroyed"}
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password_hash, :password_digest, :location)
   end
 end
