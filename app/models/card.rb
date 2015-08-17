f799fc5506549347d78a88d0a9175a5f31710a7d
Matthew Villwock <mtvillwock@gmail.com>
2015-08-16 19:46:00 -0700

    add has_many tasks, validate for list_id

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,20 @@
 class Card < ActiveRecord::Base
   belongs_to :list
   has_many :movements
-  has_many :statuses
+  has_many :tasks
 
-  validates_presence_of :organization_name
+  validates_presence_of :organization_name, :list_id
+
+  # schema attributes
+      # :list_id,
+      # :organization_name,
+      # :organization_summary,
+      # :position_applied_for,
+      # :position_description,
+      # :advocate,
+      # :tech_stack,
+      # :recent_articles,
+      # :glassdoor_rating,
+      # :title,
+      # :description
 end
