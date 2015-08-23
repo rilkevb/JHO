8d8983e6ecdd99630d166de9b3b1d3844a98c512
Matthew Villwock <mtvillwock@gmail.com>
2015-08-22 19:46:29 -0700

    WIP; attempting workaround with callback

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,9 +1,9 @@
 class MovementsController < ApplicationController
   def create
     card = Card.where(id: params[:card_id]).first
-    movement = card.movements.new(current_list: card.list.name,
+    movement = card.movements.new(
                                   # The below breaks the tests
-                                  # current_list: params[:current_list],
+                                  current_list: params[:current_list],
                                   card_id: card.id)
     if movement.save
       render json: movement, status: 200
