653c543141c917ad11df6fe51a723b4142530e11
Matthew Villwock <mtvillwock@gmail.com>
2015-08-25 14:55:45 -0700

    add #update else spec, tweak others spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,10 +9,11 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "POST #create" do
+
     context "when is successfully created" do
       before(:each) do
-        @user_attributes = { name: "Jon Snow", email: "north@thewall.com", password: "knowNothing", password_confirmation: "knowNothing" }
-        post :create, user: @user_attributes
+        @valid_attributes = { name: "Jon Snow", email: "north@thewall.com", password: "knowNothing", password_confirmation: "knowNothing" }
+        post :create, user: @valid_attributes
       end
 
       it "should add to the user count" do
@@ -30,8 +31,8 @@ RSpec.describe UsersController, type: :controller do
 
     context "when is not created" do
       before(:each) do
-        @invalid_user_attributes = { name: "foo", email: "bar", password: "baz", password_confirmation: "baz" }
-        post :create, { user: @invalid_user_attributes }
+        @invalid_attributes = { name: "foo", email: "bar", password: "baz", password_confirmation: "baz" }
+        post :create, { user: @invalid_attributes }
       end
 
       it { should respond_with 422 }
@@ -48,24 +49,34 @@ RSpec.describe UsersController, type: :controller do
         @user = User.create({ name: "Lana Kane", email: "Lana@isis.com", password: "dangerZone", password_confirmation: "dangerZone" })
         put :update, id: @user, user: { name: "Sterling Mallory Archer", email: "sterling@isis.com", password: "burtReynolds", password_confirmation: "burtReynolds"}
       end
-      it "has a success status code" do
-        expect(response).to have_http_status(:success)
+
+      it { is_expected.to respond_with 200 }
+
+      it "renders a JSON of the updated user" do
+        body = response.body
+        json_user = User.where(id: @user.id).first.to_json
+        expect(body).to eql(json_user)
       end
-      # could be more specific the updates here
     end
+
     context "when is not updated" do
-      # before(:each) do
-      #   @user = User.create({ name: "Lana Kane", email: "Lana@isis.com", password: "dangerZone", password_confirmation: "dangerZone" })
-      #   put :update, id: @user, user: { name: "Lana Kane", email: "Lana@isis.com", password: "dangerZone", password_confirmation: "dangerZone" }
-      # end
+      before(:each) do
+        @user = User.create({ name: "Lana Kane", email: "Lana@isis.com", password: "dangerZone", password_confirmation: "dangerZone" })
+        @invalid_attributes = { name: nil, email: "foo", password: "bar", password_confirmation: "bar" }
+        put :update, id: @user.id, user: @invalid_attributes
+      end
 
-      # shouldn't this be 304 for not modified?
-      # it { should respond_with 304 }
-      # it { should respond_with 200 }
+      it { is_expected.to respond_with 422 }
 
-      # it "re-renders the edit template" do
-      #   expect(response).to render_template(:edit)
-      # end
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:name]).to include "can't be blank"
+      end
     end
   end
 
@@ -74,6 +85,7 @@ RSpec.describe UsersController, type: :controller do
       before(:each) do
         @user = User.create(name: "Agent J", email: "Jay@MIB.com", password: "NoisyCricket", password_confirmation: "NoisyCricket")
       end
+
       it "changes the user count by -1" do
         expect{
           delete :destroy, id: @user
@@ -93,10 +105,28 @@ RSpec.describe UsersController, type: :controller do
     end
 
     context "when it fails to find a user to destroy" do
-      it "renders a json error" do
+      before(:each) do
         delete :destroy, id: "foo"
+      end
+
+      it  { is_expected.to respond_with 422 }
+
+      it "renders a json error" do
+        body = response.body
+        response = JSON.parse(body, symbolize_names: true)
+        expect(response).to have_key(:errors)
+      end
+
+      it "explains why the user was not found" do
         destroy_response = JSON.parse(response.body, symbolize_names: true)
-        expect(destroy_response[:errors][:user_id]).to include "not found"
+        expect(destroy_response[:errors][:id]).to include "not found"
+      end
+
+
+      it "changes the user count by 0" do
+        expect{
+          delete :destroy, id: "foo"
+        }.to change{User.count}.by(0)
       end
     end
   end
