5c26be36e7b44e80084cd19df2fc0db4cd0e3c02
Matthew Villwock <mtvillwock@gmail.com>
2015-08-27 00:32:05 -0700

    DnD works; can't drag to empty list

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -44,6 +44,35 @@
             done: false
         }];
 
+        // .dragoverCallback = function(event, index, external, type)
+        todoList.dragoverCallback = function(event, index, external, type) {
+            // todoList.logListEvent('dragged over', event, index, external, type);
+            console.log("dragged over callback");
+        };
+
+        // .dropCallback = function(event, index, item, external, type, allowedType) {
+        todoList.dropCallback = function(event, index, external, type) {
+            console.log("drop callback");
+            // todoList.logListEvent('dropped at', event, index, external, type);
+            // if (external) {
+            //     if (allowedType === 'itemType' && !item.label) return false;
+            //     if (allowedType === 'containerType' && !angular.isArray(item)) return false;
+            // }
+            // return item;
+        };
+
+
+        todoList.logEvent = function(message, event) {
+            console.log(message, '(triggered by the following', event.type, 'event)');
+            console.log(event);
+        };
+
+        todoList.logListEvent = function(action, event, index, external, type) {
+            var message = external ? 'External ' : '';
+            message += type + ' element is ' + action + ' position ' + index;
+            todoList.logEvent(message, event);
+        };
+
         todoList.addTodo = function() {
             todoList.todos.push({
                 text: todoList.todoText,
