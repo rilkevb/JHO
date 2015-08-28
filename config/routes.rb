412946976f3e9a7c6a35a3022cac9077339096a7
Matthew Villwock <mtvillwock@gmail.com>
2015-08-27 20:15:27 -0700

    revert to non-namespace

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,24 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
 
+  # format: json allows us to not have to specify the format for API requests
+  # e.g., http://www.example.com/api/v1/projects.json
+  # vs http://www.example.com/api/v1/projects
+  # namespace :api, defaults: {format: 'json'} do
+  #   namespace :v1 do
+  #     resources :sessions, only: [:create]
+  #     resources :boards, only: [:show, :index, :create, :update, :destroy]
+  #     resources :users, only: [:create, :update, :destroy]
+  #     resources :board_members, only: [:create, :update, :destroy]
+  #     resources :card_assignments, only: [:create, :update, :destroy]
+  #     resources :lists, only: [:create, :update, :destroy]
+  #     resources :cards, only: [:create, :update, :destroy] do
+  #       resources :movements, only: [:create, :update, :destroy]
+  #       resources :tasks, only: [:index, :create, :update, :destroy]
+  #     end
+  #   end
+  # end
+
   # Refactor to this later
   # with_options only: [:create, :update, :delete] do |list_only|
   #   list_only.resources :board_members
@@ -23,22 +41,15 @@ Rails.application.routes.draw do
   #   list_only.resources :movements
   # end
 
-  # format: json allows us to not have to specify the format for API requests
-  # e.g., http://www.example.com/api/v1/projects.json
-  # vs http://www.example.com/api/v1/projects
-  namespace :api, defaults: {format: 'json'} do
-    namespace :v1 do
-      resources :sessions, only: [:create]
-      resources :boards, only: [:show, :index, :create, :update, :destroy]
-      resources :users, only: [:create, :update, :destroy]
-      resources :board_members, only: [:create, :update, :destroy]
-      resources :card_assignments, only: [:create, :update, :destroy]
-      resources :lists, only: [:create, :update, :destroy]
-      resources :cards, only: [:create, :update, :destroy] do
-        resources :movements, only: [:create, :update, :destroy]
-        resources :tasks, only: [:index, :create, :update, :destroy]
-      end
-    end
+  resources :users, only: [:create, :update, :destroy]
+  resources :sessions, only: [:create]
+  resources :board_members, only: [:create, :update, :destroy]
+  resources :card_assignments, only: [:create, :update, :destroy]
+  resources :boards, only: [:show, :index, :create, :update, :destroy]
+  resources :lists, only: [:create, :update, :destroy]
+  resources :cards, only: [:create, :update, :destroy] do
+    resources :movements, only: [:create, :update, :destroy]
+    resources :tasks, only: [:index, :create, :update, :destroy]
   end
 
 
