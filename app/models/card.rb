3056a16461b7dae26a4b40c43859784d3ea43b09
Matthew Villwock <mtvillwock@gmail.com>
2015-09-15 13:00:34 -0700

    WIP; need to write set_next_task

--- app/models/card.rb
+++ app/models/card.rb
@@ -9,35 +9,25 @@ class Card < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3 }
 
   def recalculate_priority
-    puts "Before recalculation the card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     case self.list.position_id
     when 0
       self.update(priority: 1) # Interested
-      puts "&&&&&& IN CASE STATEMENT ******"
-      puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 1
       self.update(priority: 2) # Find Advocate - In progress
-      puts "&&&&&& IN CASE STATEMENT ******"
-      puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 2
       self.update(priority: 1) # Apply - Done
-      puts "&&&&&& IN CASE STATEMENT ******"
-      puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 3
       self.update(priority: 2) # Apply - In progress
-      puts "&&&&&& IN CASE STATEMENT ******"
-      puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 4
       self.update(priority: 1) # Apply - Done
-      puts "&&&&&& IN CASE STATEMENT ******"
-      puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 5
       self.update(priority: 3) # Interview
-      puts "&&&&&& IN CASE STATEMENT ******"
-      puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     end
     # Will need to extend this later if we want more lists.
-    p self.attributes
+  end
+
+  def set_next_task
+    # pending
   end
 
   # schema attributes
