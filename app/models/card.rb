bb58f93de359380a5caf30b0c4a03e8115162a55
Matthew Villwock <mtvillwock@gmail.com>
2015-09-15 12:46:47 -0700

    WIP;

--- app/models/card.rb
+++ app/models/card.rb
@@ -9,25 +9,31 @@ class Card < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3 }
 
   def recalculate_priority
-    puts "Card: \n\n #{self.inspect} \n\n with List: \n\n #{self.list.inspect}"
+    puts "Before recalculation the card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     case self.list.position_id
     when 0
       self.update(priority: 1) # Interested
+      puts "&&&&&& IN CASE STATEMENT ******"
       puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 1
       self.update(priority: 2) # Find Advocate - In progress
+      puts "&&&&&& IN CASE STATEMENT ******"
       puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 2
       self.update(priority: 1) # Apply - Done
+      puts "&&&&&& IN CASE STATEMENT ******"
       puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 3
       self.update(priority: 2) # Apply - In progress
+      puts "&&&&&& IN CASE STATEMENT ******"
       puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 4
       self.update(priority: 1) # Apply - Done
+      puts "&&&&&& IN CASE STATEMENT ******"
       puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     when 5
       self.update(priority: 3) # Interview
+      puts "&&&&&& IN CASE STATEMENT ******"
       puts "Card's new list is #{self.list.name} at position: #{self.list.position_id} and the priority is #{self.priority}"
     end
     # Will need to extend this later if we want more lists.
