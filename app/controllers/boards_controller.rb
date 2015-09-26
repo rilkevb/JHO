7b366b736b4c38df9857d01fdb8cddfee4d809fd
Matthew Villwock <mtvillwock@gmail.com>
2015-09-25 17:58:27 -0700

    WIP; compiling tasks in the json

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,7 +11,9 @@ class BoardsController < ApplicationController
 
     board = @current_user.boards.first
     lists = board.lists
-    cards = lists.map { |list| list.cards }
+    cards = lists.map { |list| list.cards } # collection of collection of cards
+    flat_cards = cards.flatten!
+    ap tasks = flat_cards.map { |card| card.tasks }
 
     # create a hash of the board's attributes
     board_hash = board.attributes
@@ -20,14 +22,17 @@ class BoardsController < ApplicationController
     # build out nested board hash by adding lists and cards
     lists.each_with_index do |list, index|
       board_hash["lists"] << list.attributes
-      board_hash["lists"][index]['cards'] = list.cards
+      board_hash["lists"][index]["cards"] = list.cards
+      cards.each_with_index do |card, i|
+        board_hash["lists"][index]["cards"][i]["tasks"] = card.tasks
+      end
     end
 
     # refactoring for efficiency
     # board.lists.include(:cards).to_json
 
     # definitely should refactor this to use ActiveModel::Serializer
-    render json: { board: board_hash, lists: lists, cards: cards.flatten! }
+    render json: { board: board_hash, lists: lists, cards: cards.flatten!, tasks: tasks.flatten! }
   end
 
   def create
